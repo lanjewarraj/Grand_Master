723478dbf5dc0e1a319d17991453e48de0e88f2c
raj lanjewar <55983199+lanjewarraj@users.noreply.github.com>
2020-06-27 14:03:54 +0000

    working on navigation

--- js/script.js
+++ js/script.js
@@ -21,4 +21,34 @@ $(function (){
 
 $(window).on('load',function (){
 	$("#rank1").addClass("animated bounceIn");
-})
+});
+
+
+/*show and hide Navigation on scroll */
+$(function () {
+
+    //on page load
+    showHideNav();
+
+    $(window).scroll(function(){
+
+        //window scroll
+        showHideNav();
+    });
+
+    //show hide nav on page load
+    function showHideNav(){
+        if(
+            $(window).scrollTop()>200){
+            //show white navigation bar  
+            $("nav").addClass("white-nav-top");
+        }
+
+        else{
+
+            //hide navigation bar
+            $("nav").removeClass("white-nav-top");
+
+        }
+    }
+});
