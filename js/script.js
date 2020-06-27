6a68eaa5b8b74822f246d1534cefc7a33e294171
raj lanjewar <55983199+lanjewarraj@users.noreply.github.com>
2020-06-27 18:53:21 +0000

    DONE mobile navigation

--- js/script.js
+++ js/script.js
@@ -52,3 +52,16 @@ $(function () {
         }
     }
 });
+
+/*Mobile menu*/
+$(function(){
+    /*show mobile nav*/
+    $('#mobile-nav-open-btn').click(function(){
+        $(".mobile-nav").css("height","100%");
+});
+    /*hide mobile nav*/
+    $("#mobile-nav-close-btn,.mobile-nav a").click(function(){
+        $(".mobile-nav").css("height","0%");
+    });
+});
+    
